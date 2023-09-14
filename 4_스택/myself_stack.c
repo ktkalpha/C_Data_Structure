296342ca26cfdcea3394f16ea7e9e6a93caed75d
ktkalpha <ktkalpha@naver.com>
2023-09-14 20:07:18 +0900

    add my stack 구현

---
+++ 4_스택/myself_stack.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+// 이 코드는 책의 구현에서 가져 옴
+// ------------------------
+#define STACK_MAX 100  // ADT 4.1 size
+
+typedef int element;
+
+element stack[STACK_MAX];  // create(size)
+// ------------------------
+
+int top = -1;
+
+int is_full();
+int is_empty();
+int push(element item);
+int pop();
+int peek();
+
+int main() {
+    printf("hello");
+}
+
+int is_full() {
+    if (top == STACK_MAX - 1)
+        return 1;
+    else
+        return 0;
+}
+int is_empty() {
+    if (top == -1)
+        return 1;
+
+    else
+        return 0;
+}
+int push(element item) {
+    if (is_full())
+        return -1;
+    else
+        stack[++top] = item;
+}
+int pop() {
+    if (is_empty())
+        return -1;
+    else
+        stack[top--] = 0;
+    return stack[top];
+}
+int peek() {
+    if (is_empty())
+        return -1;
+
+    else
+        return stack[top];
+}
